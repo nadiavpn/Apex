166b70ca83bd79d636e7e58993966efd77f201c7
nadiavpnstore <166901582+nadiavpn@users.noreply.github.com>
2024-07-14 11:09:32 +0700

    Add files via upload

--- files/cf.sh
+++ files/cf.sh
@@ -1,11 +1,6 @@
 #!/bin/bash# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Gemilangkinasih࿐
-# Email      » gemilangkinasih@gmail.com
-# telegram   » https://t.me/gemilangkinasih
-# whatsapp   » wa.me/+628984880039
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# Gemilangkinasih࿐
 
 #random
 apt install jq curl -y
@@ -16,11 +11,11 @@ echo ""
 echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
-read -rp "Input Domain Name. Example ( kontol12 ): " -e sub
-DOMAIN=serverpremium.biz.id
-SUB_DOMAIN=${sub}.serverpremium.biz.id
-CF_ID=vscobangst111@gmail.com
-CF_KEY=4a912a2d56a1b3837d48751da5334b74a4fb8
+read -rp "Input Domain Name. Example ( nadia ): " -e sub
+DOMAIN=vpnstore28.my.id
+SUB_DOMAIN=${sub}.vpnstore28.my.id
+CF_ID=vpnstore28vip@gmail.com
+CF_KEY=8f796a2271246ed60170fe5b78b7d6eb7d4c2
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
