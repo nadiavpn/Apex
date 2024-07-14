f29d94f9fdfcb813d44e29efdef7eeb91c9d6f3b
nadiavpnstore <166901582+nadiavpn@users.noreply.github.com>
2024-07-14 12:23:13 +0700

    Add files via upload

--- ubu20-deb10-stable.sh
+++ ubu20-deb10-stable.sh
@@ -564,12 +564,12 @@ print_success "Limit IP Service"
 function ssh_slow(){
 clear
 # // Installing UDP Mini
-#print_install "Memasang modul SlowDNS Server"
-#    wget -q -O /tmp/nameserver "${REPO}files/nameserver" >/dev/null 2>&1
-#    chmod +x /tmp/nameserver
-#    bash /tmp/nameserver | tee /root/install.log
-# print_success "SlowDNS"
-#}
+print_install "Memasang modul SlowDNS Server"
+    wget -q -O /tmp/nameserver "${REPO}files/nameserver" >/dev/null 2>&1
+    chmod +x /tmp/nameserver
+    bash /tmp/nameserver | tee /root/install.log
+ print_success "SlowDNS"
+}
 
 clear
 function ins_SSHD(){
